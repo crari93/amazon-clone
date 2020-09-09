79dbdc05e08d04805e67b678cfc33b6e4c928504
crari93 <33108087+crari93@users.noreply.github.com>
2020-09-09 11:00:21 -0600

    Part 4 of development

--- src/Home.js
+++ src/Home.js
@@ -13,10 +13,55 @@ export default function Home() {
         ></img>
 
         <div className="home__row">
-          <Product />
+          <Product
+            id="123156"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="123157"
+            title="Kenwood Mix Stand Miser for Bakin, Stylish Kitchen Mixer with K-beater, dough Hook and Wisk, 5 Litre Glass Bowl"
+            price={239.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            rating={4}
+          />
         </div>
-        <div className="home__row">{/* */}</div>
-        <div className="home__row">{/* */}</div>
+        <div className="home__row">
+          <Product
+            id="123158"
+            title="Amazon Echo (3rd generation) Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            rating={5}
+          />
+
+          <Product
+            id="123160"
+            title="New Apple Ipad Pro (12.9-inch, Wifi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L.AC_SX385_.jpg"
+            rating={4}
+          />
+
+          <Product
+            id="123160"
+            title="FitBit band"
+            price={87.95}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={4}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123159"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            rating={3}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-export default function Product() {
+export default function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
       </div>
+      <img src={image} alt="IMG_product"></img>
+      <button>Add to Basket</button>
     </div>
   );
 }
