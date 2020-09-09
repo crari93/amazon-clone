55e7159b1a8ebb48e605d30d4b29a59202d9986e
crari93 <33108087+crari93@users.noreply.github.com>
2020-09-09 16:49:20 -0600

    Part 7 of development

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "../Styles/Product.css";
+import { useStateValue } from "../StateProvider";
 
 export default function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //send tha data to the basket
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +36,7 @@ export default function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="IMG_product"></img>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
