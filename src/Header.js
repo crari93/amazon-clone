f8cb5656d364a48e37ba04ec7bdf955aec40401a
crari93 <33108087+crari93@users.noreply.github.com>
2020-09-08 22:39:00 -0600

    Part 2 of development

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ".*Header.css";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 export default function Header() {
   return (
@@ -10,18 +12,29 @@ export default function Header() {
       />
 
       <div className="header__search">
-        <input className="header__searchInput" type="text" /> {/*Logo*/}
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
         <div className="header__option">
-            <span className="header__optionLineOne">Hello Ariel</span>
+          <span className="header__optionLineOne">Hello Ariel</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
-        <div className="header__option">
 
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
-        <div className="header__option">
 
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
