2c4c990d41e61cf4dde35fa435ebe7324e70370b
crari93 <33108087+crari93@users.noreply.github.com>
2020-09-10 12:22:11 -0600

    Part 12 of development

--- src/reducer.js
+++ src/reducer.js
@@ -14,10 +14,18 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
     case "REMOVE_FROM_BASKET":
-      return {
-        ...state,
-        basket: [...state.basket, action.item],
-      };
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove the item (id: ${action.id}) as its not in the basket!`
+        );
+      }
+      return { ...state, basket: newBasket };
     default:
       return state;
   }
